59bb230808bd9d4c06714402ce8f3702b2682fd1
gpain1999 <guillaume.pain9@gmail.com>
2024-12-20 11:40:31 +0100

    all

--- pages/5_PLAYER_ANALYSIS.py
+++ pages/5_PLAYER_ANALYSIS.py
@@ -854,7 +854,7 @@ for col in stats_columns:
         # Vérification des données pour 'NO'
         if not data_indiv3[data_indiv3['WIN'] == 'NO'][col].empty:
 
-            avg_no = data_indiv3[data_indiv3['WIN'] == 'NO'][col].mean().round(3)
+            avg_no = data_indiv3[data_indiv3['WIN'] == 'NO'][col].mean().round(2)
         else:
             avg_no = 0
 
